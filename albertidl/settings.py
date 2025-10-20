e5760b6c45fe67f0a74f93162fba574e5b52c8d0
Franco Alberti <albertifranco1997@hotmail.com>
2025-10-20 17:14:55 -0300

    csrf panel

--- albertidl/settings.py
+++ albertidl/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://ec2-18-222-79-144.us-east-2.compute.amazonaws.com',
+    'https://albertidl.vercel.app',  # si tu frontend está ahí también
+]
+
 
 # Application definition
 
